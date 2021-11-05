e4b74a1fb5140df09498e6d5ea161dc4050a994b
OndraKonicek <konicekondra@gmail.com>
2021-11-05 10:36:25 +0100

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [flyFrom, setFlyFrom] = useState("");
+  const [flyTo, setFlyTo] = useState("");
+  const [dateFrom, setDateFrom] = useState("");
+  const [dateTo, setDateTo] = useState("");
+
+
+  async function fetchData() {
+    const response = await fetch (`https://api.skypicker.com/flights`);
+    const results = await response.json();
+    console.log(results);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>HELLO</div>
+      
     </div>
   );
 }
